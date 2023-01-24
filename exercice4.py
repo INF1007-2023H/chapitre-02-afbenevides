61a38ad4c4c76da9e65d2275ee1e3601f5e11fad
Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>
2023-01-24 16:53:02 -0500

    [ADD] exercice4 complété
    Signed-off-by: Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>

--- exercice4.py
+++ exercice4.py
@@ -3,11 +3,16 @@ import math
 
 def calculerPosition(positionInitiale, vitesseInitiale, duree, vitesseFinale):
     # TODO faites les calculs intermediaires, vous pouvez initialiser des variables locales.
+    secInHeures = 60*60
+    metreInKilometre = 1000
+
+    deltaPosition = ((vitesseInitiale * metreInKilometre + vitesseFinale * metreInKilometre) * (duree / secInHeures) / 2)
 
     # TODO calculer la position finale, assigner la valeur à la variable "positionFinale"
-    positionFinale =
+    positionFinale = positionInitiale + deltaPosition
 
     return positionFinale
+
 
 if __name__ == '__main__':
     positionInitiale = int(input("Entrez la position initiale en mètre: "))
