2a87c7ee4abaa83af5c780e4fd2ad65e546d75bf
Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>
2023-01-24 15:43:57 -0500

    [ADD] exercice 1 complété
    Signed-off-by: Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>

--- exercice1.py
+++ exercice1.py
@@ -2,8 +2,31 @@ def fizzBuzz(n):
     # TODO imprimer la chaine de caractère appropriée avec la fonction print().
     #  Assigner ensuite la valeur à la variable resultat et retourner celle-ci avec le mot-clé return.
 
-    resultat =
+    # véfiier si est multiple de 3
+    if n % 3 == 0:
+        is3multiple = True
+    else:
+        is3multiple = False
+
+    # véfiier si est multiple de 5
+    if n % 5 == 0:
+        is5multiple = True
+    else:
+        is5multiple = False
+
+    # véfiier n est dans quel cas et imprimer le bon output
+    if is3multiple and is5multiple:
+        resultat ="fizzbuzz"
+    elif is3multiple:
+        resultat = "fizz"
+    elif is5multiple:
+        resultat = "buzz"
+    else:
+        resultat = n
+
+    print(resultat)
     return resultat
+
 
 if __name__ == '__main__':
     n = int(input("indiquez le nombre: "))
