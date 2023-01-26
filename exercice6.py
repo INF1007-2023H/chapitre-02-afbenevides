dd27d6e60c8f8df99ea0341510add8c74570e810
Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>
2023-01-25 23:00:44 -0500

    [ADD] exercice 6 terminé
    Signed-off-by: Alexandre Ferreira Benevides <alexandre.ferreira.benevides@gmail.com>

--- exercice6.py
+++ exercice6.py
@@ -6,30 +6,33 @@ import numpy as np
 def trouverAngle(nombreComplexe):
     return np.angle(nombreComplexe, deg=True)
 
+
 def trouverModule(nombreComplexe):
     # TODO: Calculer le module du nombre complexe et l'assigner dans "module"
-    module =
+    module = math.sqrt(nombreComplexe.real ** 2 + nombreComplexe.imag ** 2)
 
     return module
 
 
-
 def effectuerRotation(nombreComplexe, angle_rotation, trouverModule):
-
     module = trouverModule(nombreComplexe)
     angle = trouverAngle(nombreComplexe)
 
     # TODO: Afficher le module et l'angle du nombre complexe (3 decimales de précision)
-
+    print("module: {:.3f} et angle de {:.3f} degrés ".format(module, angle))
 
     # TODO: Calculer le nouveau nombre complexe après rotation, assigner le nouveau nombre complexe à la variable 'resultat'
-
-    resultat =
+    cos= math.cos(math.radians(angle_rotation))
+    sin= math.sin(math.radians(angle_rotation))
+    rot_complexe = str(cos) + "+" + str(sin) + "j"
+    #print(rot_complexe)
+    resultat = nombreComplexe * complex(rot_complexe)
 
     nouveauModule = trouverModule(resultat)
     nouvelAngle = trouverAngle(resultat)
 
     # TODO : Afficher le nouveau module et le nouvel angle du nombre complexe après rotation (3 decimales de précision)
+    print("Nouveau module: {:.3f} et nouvel angle de {:.3f} degrés ".format(nouveauModule, nouvelAngle))
 
     return resultat
 
@@ -37,6 +40,7 @@ def effectuerRotation(nombreComplexe, angle_rotation, trouverModule):
 def dessiner(number, label):
     if number != None:
         plt.polar([0, math.radians(trouverAngle(number))], [0, trouverModule(number)], marker='o', label=label)
+
 
 if __name__ == '__main__':
     nombre = complex(input("Veuillez entrer un nombre complexe de votre choix sous la forme a+bj (exemple: 1+2j): "))
